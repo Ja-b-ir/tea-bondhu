597eeb0a65c09aab60af269c5933b65b9e374811
Jabir <jabir.codescriptors@gmail.com>
2026-08-04 03:27:05 +0600

    Update page.jsx

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -49,7 +49,7 @@ const MEMBERS = [
   },
   {
     id: "jabir",
-    photo: "/tea-bondhu/founder.jpg",
+    photo: "/public/founder.jpg",
     isSupervisor: false,
     en: {
       name: "Md Jabir Hossain",
@@ -68,7 +68,7 @@ const MEMBERS = [
   },
   {
     id: "risha",
-    photo: "/tea-bondhu/risha.jpeg",
+    photo: "/public/risha.jpeg",
     isSupervisor: false,
     en: {
       name: "Syada Rafia Bari Risha",
